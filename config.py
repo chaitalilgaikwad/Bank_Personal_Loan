9acfd95e749c85dd3a8fb1b8083c9c1fc5351899
chaitalilgaikwad <chaitaligaikwad73@gmail.com>
2024-02-19 23:07:31 +0530

    config file uploaded

---
+++ config.py
@@ -0,0 +1,3 @@
+MODEL_FILE_PATH = r"E:\1. Velocity\Data Science practice\All_Model_Testing_Data\Logistic_Bank_Loan\artifacts\linear_regression.model.pkl"
+PORT_NUMBER = 8080
+COLUMN_DATA_JSON = r"artifacts\column_data.json"
